982061f733d3bd33f445326a19b21025cbab5dce
sarfrazak7 <sarfrazak@gmail.com>
2026-09-07 00:07:13 -0700

    Fix word distribution and placement

--- src/pages/Crossword3DPage.tsx
+++ src/pages/Crossword3DPage.tsx
@@ -3,7 +3,7 @@ import { RotateCw } from 'lucide-react';
 import BackToHomeButton from '@/components/BackToHomeButton';
 import { getDeviceId } from '@/game/crossword3dPlayerStats';
 
-const GAME_VERSION = '20260907-4';
+const GAME_VERSION = '20260907-5';
 const TOP_BAR = 66;
 
 export default function Crossword3DPage() {
