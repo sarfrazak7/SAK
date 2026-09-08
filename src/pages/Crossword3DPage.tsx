32b17971349dd7477603fae341dfe24875d78119
sarfrazak7 <sarfrazak@gmail.com>
2026-09-07 17:02:23 -0700

    Update Crossword 3D layout and styles

--- src/pages/Crossword3DPage.tsx
+++ src/pages/Crossword3DPage.tsx
@@ -3,7 +3,7 @@ import { RotateCw } from 'lucide-react';
 import BackToHomeButton from '@/components/BackToHomeButton';
 import { getDeviceId } from '@/game/crossword3dPlayerStats';
 
-const GAME_VERSION = '20260907-5';
+const GAME_VERSION = '20260908-1';
 const TOP_BAR = 66;
 
 export default function Crossword3DPage() {
